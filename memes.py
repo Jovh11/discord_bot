9b8b1c6562ed46d13b5ffc4c7ff6660e1224419d
Jovh11 <93451967+Jovh11@users.noreply.github.com>
2022-08-30 15:58:48 -0400

    Update memes.py

--- memes.py
+++ memes.py
@@ -88,8 +88,6 @@ async def quote_this(ctx):
     all_quote_df = all_quote_df.drop((all_quote_df.columns[0]), axis=1).reset_index(drop=True)
     text = await ctx.channel.fetch_message(ctx.message.reference.message_id)
     name = text.author.name
-    name = str(name)
-    name = name[0:-5]
     print(type(name))
     quote = text.content
     new_quote = {}
