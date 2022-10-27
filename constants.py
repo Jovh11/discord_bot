c8066a7523fd3f361a855dbbf8a44c5c355b0600
ShanksLeftArm <shanks.left.arm44@gmail.com>
2022-10-27 10:30:30 -0400

    Structural Changes for data storage and constants file for paths

---
+++ constants.py
@@ -0,0 +1,5 @@
+RESOURCES_PATH = './Resources'
+MEMES_PATH = '{}{}'.format(RESOURCES_PATH, '/Memes')
+REACTIONS_PATH = '{}{}'.format(RESOURCES_PATH, '/Reactions')
+QUOTES_PATH = '{}{}'.format(RESOURCES_PATH, '/quote_df.csv')
+WIN_SLIPS_PATH = '{}{}'.format(REACTIONS_PATH, '/win.jpg')
