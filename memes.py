8b562813a910f6c565a1c75de5b1efdf81871e46
Jovh11 <93451967+Jovh11@users.noreply.github.com>
2023-01-03 18:26:59 -0500

    Update memes.py

--- memes.py
+++ memes.py
@@ -275,21 +275,19 @@ async def eight(ctx):
     quest_msg = await bot.wait_for('message', check=check)
     question = quest_msg.content
     name = quest_msg.author.name
-        
-    if ctx.message.reference is not None:
-        responses = ['It is certain.', 'It is decidedly so.', 'Without a doubt.', 'Yes definitely.', 'You may rely on it.', 'As I see it, yes.', 'Most likely.', 'Outlook good.', 'Yes.', 'Signs point to yes.', 'Reply hazy try again.', 'Ask again later.', 'Better not tell you now.', 'Cannot predict now.', 'Concentrate and ask again.', "Don't count on it.", 'My reply is no.', 'My sources say no.', 'Outlook not so good.', 'Very doubtful.']
-        love_response = "rub Dunn's stomach and ask again and all shall be revealed."
-        ign_response = 'My sources say that consulting ign is a mistake.'
-        ryan_response = 'Ryan is always right.'
-        if question.contains('love'):
-            await ctx.send(f'{name} my prediction is that you should {love_response}')
-        elif question.contains(' ign '):
-            await ctx.send(f'{name} {ign_response}')
-        elif question.contains('Ryan'):
-            await ctx.send(f'{name} the greatest truth of this world is {ryan_response}')
-        else:
-            response = responses[random.randint(0,(len(responses) -1))]
-            await ctx.send(f'{name} the answer to your burning query is {response}')
+    responses = ['It is certain.', 'It is decidedly so.', 'Without a doubt.', 'Yes definitely.', 'You may rely on it.', 'As I see it, yes.', 'Most likely.', 'Outlook good.', 'Yes.', 'Signs point to yes.', 'Reply hazy try again.', 'Ask again later.', 'Better not tell you now.', 'Cannot predict now.', 'Concentrate and ask again.', "Don't count on it.", 'My reply is no.', 'My sources say no.', 'Outlook not so good.', 'Very doubtful.']
+    love_response = "rub Dunn's stomach and ask again and all shall be revealed."
+    ign_response = 'My sources say that consulting ign is a mistake.'
+    ryan_response = 'Ryan is always right.'
+    if question.contains('love'):
+        await ctx.send(f'{name} my prediction is that you should {love_response}')
+    elif question.contains(' ign '):
+        await ctx.send(f'{name} {ign_response}')
+    elif question.contains('Ryan'):
+        await ctx.send(f'{name} the greatest truth of this world is {ryan_response}')
+    else:
+        response = responses[random.randint(0,(len(responses) -1))]
+        await ctx.send(f'{name} the answer to your burning query is {response}')
     
 
 # @bot.event
