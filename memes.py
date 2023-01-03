b5f774d6b349e04a468b4f13bbeaa36e8dd38a79
Jovh11 <93451967+Jovh11@users.noreply.github.com>
2023-01-03 13:29:37 -0500

    Update memes.py

--- memes.py
+++ memes.py
@@ -271,7 +271,7 @@ async def eight(ctx):
         return
     if ctx.message.reference is not None:
         text = ctx.message.content
-        name = text.author.name
+        name = ctx.message.author.name
         text_list = text.split('!8ball ')
         text_parsed = text_list[1]
         responses = ['It is certain.', 'It is decidedly so.', 'Without a doubt.', 'Yes definitely.', 'You may rely on it.', 'As I see it, yes.', 'Most likely.', 'Outlook good.', 'Yes.', 'Signs point to yes.', 'Reply hazy try again.', 'Ask again later.', 'Better not tell you now.', 'Cannot predict now.', 'Concentrate and ask again.', "Don't count on it.", 'My reply is no.', 'My sources say no.', 'Outlook not so good.', 'Very doubtful.']
